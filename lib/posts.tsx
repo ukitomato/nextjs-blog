1bb997f36175c9a96cb449de50a4d649ed82d47a
Yuki Yamato <ukitomato.developer@gmail.com>
2020-09-27 15:20:56 +0900

    Update to Typescript

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -1,8 +1,9 @@
+
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-import remark from "remark";
-import html from "remark-html";
+import remark from 'remark'
+import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
@@ -22,8 +23,8 @@ export function getSortedPostsData() {
 
         // Combine the data with the id
         return {
-            id, date: undefined,
-            ...matterResult.data
+            id,
+            ...(matterResult.data as { date: string; title: string })
         }
     })
     // Sort posts by date
@@ -38,20 +39,6 @@ export function getSortedPostsData() {
 
 export function getAllPostIds() {
     const fileNames = fs.readdirSync(postsDirectory)
-
-    // Returns an array that looks like this:
-    // [
-    //   {
-    //     params: {
-    //       id: 'ssg-ssr'
-    //     }
-    //   },
-    //   {
-    //     params: {
-    //       id: 'pre-rendering'
-    //     }
-    //   }
-    // ]
     return fileNames.map(fileName => {
         return {
             params: {
@@ -61,7 +48,7 @@ export function getAllPostIds() {
     })
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
     const fullPath = path.join(postsDirectory, `${id}.md`)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
 
@@ -78,6 +65,6 @@ export async function getPostData(id) {
     return {
         id,
         contentHtml,
-        ...matterResult.data
+        ...(matterResult.data as { date: string; title: string })
     }
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import Layout from '../../components/layout'
-import {getAllPostIds, getPostData} from '../../lib/posts'
-import Head from 'next/head'
-import Date from "../../components/date";
-import utilStyles from '../../styles/utils.module.css'
 
-export default function Post({ postData }) {
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from 'next'
+
+export default function Post({
+                                 postData
+                             }: {
+    postData: {
+        title: string
+        date: string
+        contentHtml: string
+    }
+}) {
     return (
         <Layout>
             <Head>
@@ -22,7 +31,7 @@ export default function Post({ postData }) {
     )
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
     const paths = getAllPostIds()
     return {
         paths,
@@ -30,8 +39,8 @@ export async function getStaticPaths() {
     }
 }
 
-export async function getStaticProps({params}) {
-    const postData = await getPostData(params.id)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const postData = await getPostData(params.id as string)
     return {
         props: {
             postData
